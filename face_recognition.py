772b70e7c39d4eb96c6e5df3251dbd0414bb40e5
Manasa <kazamanasa9@gmail.com>
2024-02-08 00:15:47 +0530

    Initial commit

--- face_recognition.py
+++ face_recognition.py
@@ -1,68 +1,60 @@
-
+import streamlit as st
 import cv2
+from PIL import Image
 import numpy as np
-import os 
-
-recognizer = cv2.face.LBPHFaceRecognizer_create()
-recognizer.read('trainer/trainer.yml')
-cascadePath = "haarcascade_frontalface_default.xml"
-faceCascade = cv2.CascadeClassifier(cascadePath);
-
-font = cv2.FONT_HERSHEY_TRIPLEX
-
-#iniciate id counter
-id = 0
 
 
-names = [0, 1, 2, 3, 'Z', 'W'] 
+face_cascade = cv2.CascadeClassifier('haarcascade_frontalface_default.xml')
 
-# Initialize and start realtime video capture
-cam = cv2.VideoCapture(0)
-cam.set(3, 640) # set video widht
-cam.set(4, 480) # set video height
+rec=cv2.face.LBPHFaceRecognizer_create()
+rec.read("trainer.yml")
+def detect_faces(our_image):
+    img = np.array(our_image.convert('RGB'))
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    # Detect faces
+    faces = face_cascade.detectMultiScale(gray, 1.1, 4)
+    # Draw rectangle around the faces
+    name='Unknown'
+    for (x, y, w, h) in faces:
+        # To draw a rectangle in a face
+        cv2.rectangle(img, (x, y), (x + w, y + h), (255, 255, 0), 2)
+        id, uncertainty = rec.predict(gray[y:y + h, x:x + w])
+        print(id, uncertainty)
 
-# Define min window size to be recognized as a face
-minW = 0.1*cam.get(3)
-minH = 0.1*cam.get(4)
-
-while True:
-
-    ret, img =cam.read()
-   
-
-    gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-
-    faces = faceCascade.detectMultiScale( 
-        gray,
-        scaleFactor = 1.2,
-        minNeighbors = 5,
-        minSize = (int(minW), int(minH)),
-       )
-
-    for(x,y,w,h) in faces:
-
-        cv2.rectangle(img, (x,y), (x+w,y+h), (0,255,0), 2)
-
-        id, confidence = recognizer.predict(gray[y:y+h,x:x+w])
-
-        # Check if confidence is less them 100 ==> "0" is perfect match 
-        if (confidence < 100):
-            id = names[id]
-            confidence = "  {0}%".format(round(100 - confidence))
+        if (uncertainty< 53):
+            if (id == 1 or id == 3 or id == 5):
+                name = "It's me"
+                cv2.putText(img, name, (x, y + h), cv2.FONT_HERSHEY_COMPLEX_SMALL, 2.0, (0, 0, 255))
         else:
-            id = "unknown"
-            confidence = "  {0}%".format(round(100 - confidence))
-        
-        cv2.putText(img, str(id), (x+5,y-5), font, 1, (255,255,255), 2)
-        cv2.putText(img, str(confidence), (x+5,y+h-5), font, 1, (255,255,0), 1)  
-    
-    cv2.imshow('camera',img) 
+            cv2.putText(img, 'Unknown', (x, y + h), cv2.FONT_HERSHEY_COMPLEX_SMALL, 2.0, (0, 0, 255))
 
-    k = cv2.waitKey(10) & 0xff 
-    if k == 27:
-        break
 
-# Do a bit of cleanup
-print("\n [INFO] Exiting Program")
-cam.release()
-cv2.destroyAllWindows()
+    return img
+def main():
+    """Face Recognition App"""
+
+    st.title("Streamlit Tutorial")
+
+    html_temp = """
+    <body style="background-color:red;">
+    <div style="background-color:teal ;padding:10px">
+    <h2 style="color:white;text-align:center;">Face Recognition WebApp</h2>
+    </div>
+    </body>
+    """
+    st.markdown(html_temp, unsafe_allow_html=True)
+
+    image_file = st.file_uploader("Upload Image", type=['jpg', 'png', 'jpeg'])
+    if image_file is not None:
+        our_image = Image.open(image_file)
+        st.text("Original Image")
+        st.image(our_image)
+
+    if st.button("Recognise"):
+        result_img= detect_faces(our_image)
+        st.image(result_img)
+
+
+if __name__ == '__main__':
+    main()
+
